8bd56596f704e89b962de0311f405d5bcfe16398
Taras Umanskiy <Tarasmetal@gmail.com>
2023-12-06 12:58:11 +0300

    Save Files - 06.12.23 - 12:58

---
+++ GUI Tools/MarkerPresets/Default color test.lua
@@ -0,0 +1,23 @@
+-- @description MarkerGUITools Preset
+-- @author Taras Umanskiy
+-- @version 1.0
+-- @provides [nomain] .
+Intro,black
+Main,
+Verse,yellow
+Chorus,green
+Fill,lightblue
+Pre,orange
+Bridge,orange
+BDown,black
+Drop,
+Beat,
+Silent,gray
+Riff,lightgreen
+Outro,black
+-,
+Phase,white
+Idea,yellow
+New,white
+Old,brown
+Fx,cyan
